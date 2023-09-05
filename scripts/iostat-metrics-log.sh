7593d6ab3c0fa4f38ffc10fad78448fc4b8a392d
Alex Kup <116945542+Lifailon@users.noreply.github.com>
2023-09-05 14:20:34 +0300

    update

--- scripts/iostat-metrics-log.sh
+++ scripts/iostat-metrics-log.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-disk="sda"
 path="/var/log/iostat.log"
-tmp="/var/log/iostat.tmp"
 while :
 do
-rm $tmp 2> /dev/null
 date=$(date | awk '{print $3,$2,$4}')
-iostat -yh /dev/$disk 1 > $tmp &
-sleep 2
-kill %1
-tps=$(cat $tmp | grep $disk | awk '{print $1}' | sed -E "s/\..+//")
-rs=$(cat $tmp | grep $disk | awk '{print $2}')
-ws=$(cat $tmp | grep $disk | awk '{print $3}')
+sd=($(ls -l /dev | grep -wo sd.))
+iostat=$(iostat -hy /dev/sd* 1 1 | grep -w sd.)
+for d in ${sd[@]}
+do
+print=$(printf "%s\n" "${iostat[@]}" | grep $d)
+tps=$(echo $print | awk '{print $1}' | sed -E "s/\..+//")
+rs=$(echo $print | awk '{print $2}')
+ws=$(echo $print | awk '{print $3}')
 echo "$date  tps = $tps  read/s = $rs  write/s = $ws" >> $path
 cat $path | tail -n 1
-rm $tmp
+done
 sleep 5
 done
